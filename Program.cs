cb9eacbe4b339c753f2df8396185ce3fd67187ae
Ben <87502947+CodifiedDev@users.noreply.github.com>
2023-12-14 10:59:04 +1000

    Added basic web-scraping capabilities

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ARSA;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine(webScraper.getWebPage("https://old.reddit.com").Result);
---
+++ webScraper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARSA
+{
+    internal class webScraper
+    {
+        public async static Task<string> getWebPage(string url)
+        {
+            HttpClient http = new();
+            //The user agent must be set to a valid browser or the request will be rejected
+            http.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0");
+            HttpResponseMessage response = await http.GetAsync(new Uri(url));
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            //This is unparsed data, It will not be readable
+            return jsonResponse;
+        }
+    }
+}
