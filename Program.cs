59195805bef04eff54f970de1ed5c85b97025ea6
Ben <87502947+CodifiedDev@users.noreply.github.com>
2023-12-14 12:04:45 +1000

    Completed the Web Scraping component, Old.reddit.com Links can now be scraped and their titles and Content displayed

--- Program.cs
+++ Program.cs
@@ -2,5 +2,13 @@
 using ARSA;
 
 
-string webdata = webScraper.getWebPage("https://old.reddit.com/r/AITAH").Result;
-List<string> test = webScraper.getTopPostLinks(webdata).Result;
+List<string> test = webScraper.getTopPostLinks("https://old.reddit.com/r/AITAH").Result;
+for (int i = 0; i < 1; i++)
+{
+    string url = "https://old.reddit.com" + test[1];
+    var contnet = webScraper.getPostContent(url).Result;
+    foreach (var contents in contnet)
+    {
+        Console.WriteLine(contents);
+    }
+}
--- webScraper.cs
+++ webScraper.cs
@@ -19,8 +19,10 @@ namespace ARSA
             //This is unparsed data, It will not be readable
             return jsonResponse;
         }
-        public async static Task<List<string>> getTopPostLinks(string htmlDATA)
+        public async static Task<List<string>> getTopPostLinks(string url)
         {
+            //URL is required to be in the format of https://old.reddit.com/r/subreddit
+            string htmlDATA = await getWebPage(url);
             //htmlDATA will be unparsed data, preferably taken from getWebPage, It is assumed that the data is coming from a https://old.reddit.com/r/subreddit
             var doc = new HtmlDocument();
             doc.LoadHtml(htmlDATA);
@@ -40,6 +42,30 @@ namespace ARSA
                 }
             }
             return links;
+        }
+
+
+        public async static Task<List<string>> getPostContent(string url)
+        {
+            //This must ALWAYS only return a 2 value long List, with the first element being the title of the post, and the second being the body of text
+            //This assumes that the url has already been concecrated with the predeterminer of old.reddit.com, as this does not appear in the getTopPostLinks method
+            string htmlDATA = await getWebPage(url);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlDATA);
+            //The list must now be created to store the data
+            List<string> postContent = new List<string>();
+            //The title is found with the anchor of class title
+            HtmlNode titleNode = doc.DocumentNode.Descendants("a").Where(node => node.HasClass("title")).First();
+            //The title is then added to the list
+            postContent.Add(titleNode.InnerText);
+            //The body must now be found, it is located in the div of class expando
+            HtmlNode bodyNode = doc.DocumentNode.Descendants("div").Where(node => node.HasClass("expando")).First();
+            //The body node must then be converted out of HTML encoding
+            var contentbody = System.Net.WebUtility.HtmlDecode(bodyNode.InnerText);
+            //The body is then added to the list
+            postContent.Add(contentbody);
+            //The list is then returned
+            return postContent;
         }
     }
 }
