b7f81a4dd28ae8860f6303cfcc6a7d2c28bc5783
CoolSlimbo <lrabie07@outlook.com>
2023-12-20 17:07:56 +1000

    Moved to src folder. And added preformance and clarity fixes.

--- src/Program.cs
+++ src/Program.cs
@@ -3,12 +3,12 @@ using ARSA;
 
 //This is the main entrance point for the script, however it runs the actual main function in fullStackRun.cs, This may not be best practice.
 Console.WriteLine("Welcome to ARSA");
-Console.WriteLine("Please enter the subreddit you wish to use");
+Console.WriteLine("Please enter the subreddit you wish to use: (e.g. askreddit)");
 string subreddit = Console.ReadLine();
-Console.WriteLine("Please enter the path to the input file");
+Console.WriteLine("Please enter the path to the input file: (expecting .mp4)");
 string inputPath = Console.ReadLine();
-Console.WriteLine("Please enter the path to the output file");
+Console.WriteLine("Please enter the path to the output file: (expecting existing directory)");
 string outputPath = Console.ReadLine();
-Console.WriteLine("Please enter the number of videos you wish to create");
+Console.WriteLine("Please enter the number of videos you wish to create: (max 25)");
 int numberofVideos = Convert.ToInt32(Console.ReadLine());
 fullStackRun.Main(subreddit, inputPath, outputPath, numberofVideos);
--- src/webScraper.cs
+++ src/webScraper.cs
@@ -9,12 +9,12 @@ namespace ARSA
 {
     internal class webScraper
     {
-        public async static Task<string> getWebPage(string url)
+        public async static Task<string> getWebPage(Uri url)
         {
             HttpClient http = new();
             //The user agent must be set to a valid browser or the request will be rejected
             http.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0");
-            HttpResponseMessage response = await http.GetAsync(new Uri(url));
+            HttpResponseMessage response = await http.GetAsync(url);
             string jsonResponse = await response.Content.ReadAsStringAsync();
             //This is unparsed data, It will not be readable
             return jsonResponse;
@@ -22,26 +22,28 @@ namespace ARSA
         public async static Task<List<string>> getTopPostLinks(string url)
         {
             //URL is required to be in the format of https://old.reddit.com/r/subreddit
-            string htmlDATA = await getWebPage(url);
+            string htmlDATA = await getWebPage(new Uri(url));
             //htmlDATA will be unparsed data, preferably taken from getWebPage, It is assumed that the data is coming from a https://old.reddit.com/r/subreddit
             var doc = new HtmlDocument();
             doc.LoadHtml(htmlDATA);
             //HtmlAgilityPack then needs to find all p tags with the class title
             IEnumerable<HtmlNode> nodes = doc.DocumentNode.Descendants("a").Where(node => node.HasClass("title"));
             //Before all items are looped through a list is created to store all the links
-           List<string> links = new List<string>();
-            foreach (HtmlNode node in nodes)
-            {
-                //HtmlAgilityPack is used again to get the href elements of of the anchors
-                string link = node.GetAttributeValue("href", "");
-                //All advertising links must be removed, this is done by ignoring all links that contain 'https://', this is because all advertising links are external
-                if (!link.Contains("https://"))
-                {
-                    //ALL Links are added to the list, this will be trucanted to the desired amount when deciding to parse the data, not at this stage.
-                    links.Add(link);
-                }
-            }
-            return links;
+            // List<string> links = new List<string>();
+            // foreach (HtmlNode node in nodes)
+            // {
+            //     //HtmlAgilityPack is used again to get the href elements of of the anchors
+            //     string link = node.GetAttributeValue("href", "");
+            //     //All advertising links must be removed, this is done by ignoring all links that contain 'https://', this is because all advertising links are external
+            //     if (!link.Contains("https://"))
+            //     {
+            //         //ALL Links are added to the list, this will be trucanted to the desired amount when deciding to parse the data, not at this stage.
+            //         links.Add(link);
+            //     }
+            // }
+            var links = nodes.Select(node => node.GetAttributeValue("href", ""));
+            links = links.Where(node => !node.Contains("https://"));
+            return (List<string>)links;
         }
 
 
@@ -49,7 +51,7 @@ namespace ARSA
         {
             //This must ALWAYS only return a 2 value long List, with the first element being the title of the post, and the second being the body of text
             //This assumes that the url has already been concecrated with the predeterminer of old.reddit.com, as this does not appear in the getTopPostLinks method
-            string htmlDATA = await getWebPage(url);
+            string htmlDATA = await getWebPage(new Uri(url));
             var doc = new HtmlDocument();
             doc.LoadHtml(htmlDATA);
             //The list must now be created to store the data
