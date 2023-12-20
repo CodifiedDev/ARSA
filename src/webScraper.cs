31aa60f4ade174de341c0fee18631f17f3159014
CoolSlimbo <lrabie07@outlook.com>
2023-12-20 17:11:05 +1000

    Converted from list to tuple.

--- src/webScraper.cs
+++ src/webScraper.cs
@@ -19,6 +19,7 @@ namespace ARSA
             //This is unparsed data, It will not be readable
             return jsonResponse;
         }
+
         public async static Task<List<string>> getTopPostLinks(string url)
         {
             //URL is required to be in the format of https://old.reddit.com/r/subreddit
@@ -46,28 +47,21 @@ namespace ARSA
             return (List<string>)links;
         }
 
-
-        public async static Task<List<string>> getPostContent(string url)
+        public async static Task<(string, string)> getPostContent(string url)
         {
             //This must ALWAYS only return a 2 value long List, with the first element being the title of the post, and the second being the body of text
             //This assumes that the url has already been concecrated with the predeterminer of old.reddit.com, as this does not appear in the getTopPostLinks method
             string htmlDATA = await getWebPage(new Uri(url));
             var doc = new HtmlDocument();
             doc.LoadHtml(htmlDATA);
-            //The list must now be created to store the data
-            List<string> postContent = new List<string>();
             //The title is found with the anchor of class title
             HtmlNode titleNode = doc.DocumentNode.Descendants("a").Where(node => node.HasClass("title")).First();
-            //The title is then added to the list
-            postContent.Add(titleNode.InnerText);
             //The body must now be found, it is located in the div of class expando
             HtmlNode bodyNode = doc.DocumentNode.Descendants("div").Where(node => node.HasClass("expando")).First();
             //The body node must then be converted out of HTML encoding
             var contentbody = System.Net.WebUtility.HtmlDecode(bodyNode.InnerText);
-            //The body is then added to the list
-            postContent.Add(contentbody);
             //The list is then returned
-            return postContent;
+            return (titleNode.InnerText, contentbody);
         }
     }
 }
